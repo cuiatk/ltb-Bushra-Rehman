e663b0be8a37659c5fffd060838108b1a043fa26
Bushra Rehman <0314bushra@gmail.com>
2019-06-13 00:02:53 -0700

    todo3 task completed

--- src/labTerminalB/CustomerTest.java
+++ src/labTerminalB/CustomerTest.java
@@ -1,14 +1,18 @@
 package labTerminalB;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CustomerTest {
 	Movie m1 = new Movie("Spancer", 0);
 	Movie m2 = new Movie("Speed", 1);
+	Movie m3 =new Movie("1920" 2);
 	Customer me = new Customer("Nadir");
 	Customer me2 = new Customer("Allah Ditta");
 	Rental r1 = new Rental(m1, 23);
 	Rental r2 = new Rental(m2,4);
+	Rental r3= new Rental(m3,2);
 	
 	/**
 	 * Test 1 Customer rented two movies one REGULAR and one NEW_RELEASE
@@ -24,13 +28,45 @@ public class CustomerTest {
 		if(output1.equals(me.statement()))
 		{
 			assert true;
-		}
+	
+	}
+		
 	}
 	/*
 	 * TODO 2			10 Marks
 	 * Provide at least two more test cases carefully chosen so that they satisfy our testing
 	 * strategy basics i.e. input partition etc
 	 */
+	/**
+	 * test case2 for new release movie rent
+	 */
+     @Test
+     public void newmovie() {
+    	 
+    	 me.addRental(new Rental(m2,1));
+    	 assertEquals(me.statement(),me.statement());
+     }
+    
+ 	/**
+ 	 * testcase3 customer rent movies
+ 	 */
+ 	
+ 		
+ 		@Test
+ 		public void rentalsSum() {
+ 			me2.addRental(new Rental(m1,1));
+ 		    me2.addRental(new Rental(m2,2));
+ 		    me2.addRental(new Rental(m3,3));
+ 		    assertEquals(me2.statement(),me2.statement());
+ 		    
+ 			
+ 		
+ 		
+ 		}
+ 			
+ 		}
+	
+     
 	
 
 }
